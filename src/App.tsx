b8acdfae9ed8d3859a1894ff05c56f5e30e7a505
andyotarola <otarolaandy@gmail.com>
2024-04-28 12:35:04 -0500

    Fix linter

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import viteLogo from "/vite.svg"
 function App() {
 	const [count, setCount] = useState(0)
 
+	const handleClick = () => {
+		setCount((count) => count + 1)
+	}
+
 	return (
 		<>
 			<div>
@@ -18,7 +22,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button onClick={handleClick} type="button">
 					count is {count}
 				</button>
 				<p>
